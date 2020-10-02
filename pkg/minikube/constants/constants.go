746789e275544635f5fa76981cc3ff44d767a450
Predrag Rogic <prezha@users.noreply.github.com>
2020-10-02 18:40:14 +0100

    update_kubernetes_version: {"stable": "v1.19.2", "latest": "v1.19.3"}

--- pkg/minikube/constants/constants.go
+++ pkg/minikube/constants/constants.go
@@ -30,7 +30,7 @@ const (
 	DefaultKubernetesVersion = "v1.19.2"
 	// NewestKubernetesVersion is the newest Kubernetes version to test against
 	// NOTE: You may need to update coreDNS & etcd versions in pkg/minikube/bootstrapper/images/images.go
-	NewestKubernetesVersion = "v1.19.2"
+	NewestKubernetesVersion = "v1.19.3"
 	// OldestKubernetesVersion is the oldest Kubernetes version to test against
 	OldestKubernetesVersion = "v1.13.0"
 	// DefaultClusterName is the default nane for the k8s cluster
